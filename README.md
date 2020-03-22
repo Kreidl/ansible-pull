e0e1434459888f6cea970e8be65631de1b24cba9
Kreidl <lukaskienast0@gmail.com>
2020-03-22 12:31:36 +0100

    Added playbook and cronjob

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ansible-pull
+
+This repo contains a example ansible pull playbook<br/>
+
+For this a shell script should be written for the nodes (see provision.sh)<br/>
+
+Thanks to https://github.com/jpmens/n-repo<br/>
---
+++ provision.sh
@@ -0,0 +1,17 @@
+#/bin/sh
+
+#Install pip which is needed for ansible
+sudo apt-get install -y python-pip
+
+#Install ansible with pip
+sudo pip install --user ansible
+sudo apt-get install -y software-properties-common git
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt-get install -y ansible
+
+#Create directory
+sudo mkdir -p /etc/ansible
+
+cd /etc/ansible
+
+sudo git clone https://github.com/Kreidl/ansible-pull .
